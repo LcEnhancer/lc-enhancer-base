781248d3bd3002866ad4cf3ad56cbbe1975ee253
Jidcoo <65707480+Jidcoo@users.noreply.github.com>
2025-03-30 03:46:29 +0800

    Fix up groupid. (#3)
    * change project group-id.
    
    * Update pom.xml.
    
    * Rename package name.
    
    * rename package name.
    
    * change package name.

---
+++ src/main/java/io/github/lcenhancer/base/utils/OrderUtil.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright (C) 2025-2030 Jidcoo(https://github.com/jidcoo).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.github.lcenhancer.base.utils;
+
+import io.github.lcenhancer.base.interfaces.Order;
+
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * Common order util for {@link Order}.
+ *
+ * @author Jidcoo
+ * @see Order
+ * @since 1.0.0
+ */
+public class OrderUtil {
+
+    private static final Comparator<? super Order> ASC_ORDER_CMP = Comparator.comparingInt(Order::getOrder);
+
+    private static final Comparator<? super Order> DESC_ORDER_CMP = ASC_ORDER_CMP.reversed();
+
+    /**
+     * ASC sort.
+     *
+     * @param list the order-able list.
+     */
+    public static void ascSort(List<? extends Order> list) {
+        orderSort(list, ASC_ORDER_CMP);
+    }
+
+    /**
+     * DESC sort.
+     *
+     * @param list the order-able list.
+     */
+    public static void descSort(List<? extends Order> list) {
+        orderSort(list, DESC_ORDER_CMP);
+    }
+
+    /**
+     * Get asc order comparator.
+     *
+     * @return Comparator
+     */
+    public static Comparator<? super Order> ascComparator() {
+        return ASC_ORDER_CMP;
+    }
+
+    /**
+     * Get desc order comparator.
+     *
+     * @return Comparator
+     */
+    public static Comparator<? super Order> descComparator() {
+        return DESC_ORDER_CMP;
+    }
+
+    private static <Sortable> void orderSort(List<? extends Sortable> list, Comparator<? super Sortable> cmp) {
+        AssertUtil.nonNull(list, "The list cannot be null.");
+        list.sort(cmp);
+    }
+}
